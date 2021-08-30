aade4add392332faec37e0b3d81270ab5e069f4e
mubi51 <mubi.exe@gmail.com>
2021-08-30 16:55:35 +0200

    merise maj

---
+++ DonnéeTest.sql
@@ -0,0 +1,9 @@
+Catégorie : 
+
+INSERT INTO `categorie` (`IdCategorieDuSite`, `CategorieDesSites`) VALUES (NULL, 'Voiture');
+
+INSERT INTO `categorie` (`IdCategorieDuSite`, `CategorieDesSites`) VALUES (NULL, 'Plomberie');
+
+Clients : 
+
+INSERT INTO `client` (`IDClient`, `MdpClient`, `AnneedenaissanceClient`, `VilleClient`, `CodePostalClient`, `AdressClient`, `IdentifiantClient`) VALUES (NULL, '1234', '1997-08-05', 'Reims', '51100', '14 rue Camille le noir', 'masta');
