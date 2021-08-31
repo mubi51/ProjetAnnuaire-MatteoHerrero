fcee286d3101b6c836d2aed3b5ae5138983e7650
mubi51 <mubi.exe@gmail.com>
2021-08-31 11:46:57 +0200

    Requete Modif

--- DonnéeTest.sql
+++ DonnéeTest.sql
@@ -6,12 +6,21 @@ INSERT INTO `categorie` (`IdCategorieDuSite`, `CategorieDesSites`) VALUES (NULL,
 
 #Clients : 
 
-INSERT INTO `client` (`IDClient`,'NomClient','PrenomClient',`IdentifiantClient`,`AdresseMailClient`, `MdpClient`, `AnneedenaissanceClient`, `VilleClient`, `CodePostalClient`, `AdressClient`) VALUES (NULL,'Herrero','Matteo','masta','masta@mail.fr', '1234', '1997-08-05', 'Reims', '51100', '14 rue Camille le noir');
+INSERT INTO `client` (`IDClient`,`NomClient`,`PrenomClient`,`IdentifiantClient`,`AdresseMailClient`, `MdpClient`, `AnneedenaissanceClient`, `VilleClient`, `CodePostalClient`, `AdressClient`) VALUES (NULL,'Herrero','Matteo','masta','masta@mail.fr', '1234', '1997-08-05', 'Reims', '51100', '14 rue Camille le noir');
 
 #Admin :
 
 INSERT INTO `admin` (`IDAdmin`, `MdpAdmin`, `AdresseMailAdmin`, `IdentifiantAdmin`) VALUES (NULL, '1234', 'mail@mail.com', 'mamama');
 
 
+#MotCles
+
+INSERT INTO `motcle` (`IdMotCles`, `MotCles`) VALUES (NULL, 'Peugeot');
+
+#MotCles
 
 
+#Site
+
+ALTER TABLE `site` CHANGE `Image` `Image` MEDIUMBLOB NOT NULL;
+INSERT INTO `site` (`IdSite`, `approuve`, `Description`, `SiteClient`, `IDAdmin`, `IdCategorieDuSite`, `IDClient`) VALUES (NULL, '1', 'Un site', 'https//:ph-auto-net.com', '1', '1', '1');
