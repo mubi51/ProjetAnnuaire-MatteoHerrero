9d1fc6802b36d32ab319189c0575e7c0d3cf2f9d
mubi51 <mubi.exe@gmail.com>
2021-08-31 10:15:12 +0200

    Jmerise Maj V3

--- DonnéeTest.sql
+++ DonnéeTest.sql
@@ -1,9 +1,17 @@
-Catégorie : 
+#Catégorie : 
 
 INSERT INTO `categorie` (`IdCategorieDuSite`, `CategorieDesSites`) VALUES (NULL, 'Voiture');
 
 INSERT INTO `categorie` (`IdCategorieDuSite`, `CategorieDesSites`) VALUES (NULL, 'Plomberie');
 
-Clients : 
+#Clients : 
 
-INSERT INTO `client` (`IDClient`, `MdpClient`, `AnneedenaissanceClient`, `VilleClient`, `CodePostalClient`, `AdressClient`, `IdentifiantClient`) VALUES (NULL, '1234', '1997-08-05', 'Reims', '51100', '14 rue Camille le noir', 'masta');
+INSERT INTO `client` (`IDClient`,'NomClient','PrenomClient',`IdentifiantClient`,`AdresseMailClient`, `MdpClient`, `AnneedenaissanceClient`, `VilleClient`, `CodePostalClient`, `AdressClient`) VALUES (NULL,'Herrero','Matteo','masta','masta@mail.fr', '1234', '1997-08-05', 'Reims', '51100', '14 rue Camille le noir');
+
+#Admin :
+
+INSERT INTO `admin` (`IDAdmin`, `MdpAdmin`, `AdresseMailAdmin`, `IdentifiantAdmin`) VALUES (NULL, '1234', 'mail@mail.com', 'mamama');
+
+
+
+
--- MCD.sql
+++ MCD.sql
@@ -11,14 +11,13 @@ CREATE TABLE Client(
         IDClient               Int  Auto_increment  NOT NULL ,
         NomClient              Varchar (50) NOT NULL ,
         PrenomClient           Varchar (50) NOT NULL ,
-	IdentifiantClient      Varchar (50) NOT NULL ,
         AdresseMailClient      Varchar (50) NOT NULL ,
         MdpClient              Varchar (50) NOT NULL ,
         AnneedenaissanceClient Date NOT NULL ,
         VilleClient            Varchar (50) NOT NULL ,
         CodePostalClient       Int NOT NULL ,
-        AdressClient           Varchar (50) NOT NULL 
-      
+        AdressClient           Varchar (50) NOT NULL ,
+        IdentifiantClient      Varchar (50) NOT NULL
 	,CONSTRAINT Client_AK UNIQUE (IdentifiantClient)
 	,CONSTRAINT Client_PK PRIMARY KEY (IDClient)
 )ENGINE=InnoDB;
@@ -56,7 +55,6 @@ CREATE TABLE Categorie(
 CREATE TABLE Site(
         IdSite            Int  Auto_increment  NOT NULL ,
         approuve          Bool NOT NULL ,
-        MotCles           Varchar (50) NOT NULL ,
         Description       Varchar (50) NOT NULL ,
         Image             Varchar (50) NOT NULL ,
         SiteClient        Varchar (50) NOT NULL ,
@@ -69,4 +67,29 @@ CREATE TABLE Site(
 	,CONSTRAINT Site_ADMIN_FK FOREIGN KEY (IDAdmin) REFERENCES ADMIN(IDAdmin)
 	,CONSTRAINT Site_Categorie0_FK FOREIGN KEY (IdCategorieDuSite) REFERENCES Categorie(IdCategorieDuSite)
 	,CONSTRAINT Site_Client1_FK FOREIGN KEY (IDClient) REFERENCES Client(IDClient)
+)ENGINE=InnoDB;
+
+
+#------------------------------------------------------------
+# Table: MotCle
+#------------------------------------------------------------
+
+CREATE TABLE MotCle(
+        IdMotCles Int  Auto_increment  NOT NULL ,
+        MotCles   Varchar (50) NOT NULL
+	,CONSTRAINT MotCle_PK PRIMARY KEY (IdMotCles)
+)ENGINE=InnoDB;
+
+
+#------------------------------------------------------------
+# Table: MotsClesSite
+#------------------------------------------------------------
+
+CREATE TABLE MotsClesSite(
+        IdMotCles Int NOT NULL ,
+        IdSite    Int NOT NULL
+	,CONSTRAINT MotsClesSite_PK PRIMARY KEY (IdMotCles,IdSite)
+
+	,CONSTRAINT MotsClesSite_MotCle_FK FOREIGN KEY (IdMotCles) REFERENCES MotCle(IdMotCles)
+	,CONSTRAINT MotsClesSite_Site0_FK FOREIGN KEY (IdSite) REFERENCES Site(IdSite)
 )ENGINE=InnoDB;
